8d4b0e8508ce60bc575cc03c940402733ecebc00
Greg DiCristofaro <gregdicristofaro@gmail.com>
2025-03-23 10:46:55 -0400

    work towards play panel

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,113 +1,202 @@
-import Image from "next/image";
-import { Geist, Geist_Mono } from "next/font/google";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import IconButton from "@mui/material/IconButton";
+import PlayArrowIcon from '@mui/icons-material/PlayArrow';
+import ReplayIcon from '@mui/icons-material/Replay';
+import SettingsIcon from '@mui/icons-material/Settings';
+import VolumeUp from '@mui/icons-material/VolumeUp';
+import VolumeOffIcon from '@mui/icons-material/VolumeOff';
+import LinkIcon from '@mui/icons-material/Link';
+import Slider from '@mui/material/Slider';
+import { useState } from "react";
+import { Dialog, DialogTitle, DialogContent, DialogContentText, TextField, DialogActions, Button, Checkbox, FormControlLabel } from "@mui/material";
+import Grid from '@mui/material/Grid2';
+import FullscreenIcon from '@mui/icons-material/Fullscreen';
+import FullscreenExitIcon from '@mui/icons-material/FullscreenExit';
+import SkipNextIcon from '@mui/icons-material/SkipNext';
 
 export default function Home() {
+  let [value, setValue] = useState(0);
+  let [open, setOpen] = useState(false);
+
+  let handleChange = (event: Event, newValue: number | number[]) => {
+    setValue(newValue as number);
+  }
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/pages/index.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className="main-container">
+      <div className="control-panel">
+
+      <div className="volume-panel">
+            <IconButton aria-label="volume">
+              <VolumeUp />
+              {/* <VolumeOffIcon /> */}
+            </IconButton>
+            <Slider className="volume-slider" aria-label="Volume" value={value} onChange={handleChange} />
+          </div>
+
+        <div className="button-panel">
+          <IconButton aria-label="settings" onClick={() => setOpen(true)}>
+            <SettingsIcon />
+          </IconButton>
+
+          <IconButton aria-label="link">            
+            <LinkIcon />
+          </IconButton>
+
+          <IconButton aria-label="full-screen">
+            <FullscreenIcon/>
+            {/* <FullscreenExitIcon/> */}
+          </IconButton>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+
+        <div className="play-pause-panel">
+          <IconButton aria-label="replay">
+            <ReplayIcon />
+          </IconButton>
+
+          <IconButton aria-label="play">
+            <PlayArrowIcon />
+          </IconButton>
+{/* 
+          <IconButton aria-label="skip-intro">
+            <SkipNextIcon />
+          </IconButton> */}
+
+          <div></div>
+        </div>
+        
+        <div className="seeking-panel">
+          <Slider aria-label="Volume" value={value} onChange={handleChange} />
+        </div>
+      </div>
+
+      <Dialog
+        open={open}
+        onClose={() => setOpen(false)}
+        slotProps={{
+          paper: {
+            component: 'form',
+            onSubmit: (event: React.FormEvent<HTMLFormElement>) => {
+              event.preventDefault();
+              const formData = new FormData(event.currentTarget);
+              const formJson = Object.fromEntries((formData as any).entries());
+              const email = formJson.email;
+              console.log(email);
+              setOpen(false);
+            },
+          },
+        }}
+      >
+        <DialogTitle>Settings</DialogTitle>
+        <DialogContent>
+          {/* <DialogContentText></DialogContentText> */}
+          <Grid container spacing={2}>
+            <Grid size={12}>
+              <TextField
+                autoFocus
+                margin="dense"
+                id="episode"
+                name="Episode"
+                label="Episode"
+                type="text"
+                fullWidth
+                variant="standard"
+              />
+
+            </Grid>
+            <Grid size={12}>
+              <TextField
+                autoFocus
+                margin="dense"
+                id="title"
+                name="Title"
+                label="Title"
+                type="text"
+                fullWidth
+                variant="standard"
+              />
+            </Grid>
+            <Grid size={12}>
+              <TextField
+                autoFocus
+                margin="dense"
+                id="crawl"
+                name="Crawl"
+                label="Crawl"
+                type="text"
+                multiline
+                fullWidth
+                variant="standard"
+              />
+            </Grid>
+            <Grid size={12} sx={{
+              display: "flex",
+              alignContent: "baseline",
+              alignItems: "baseline"
+            }}>
+              <TextField
+                autoFocus
+                margin="dense"
+                id="intro"
+                name="Intro"
+                label="Intro"
+                type="text"
+                fullWidth
+                multiline
+                variant="standard"
+              />
+              <FormControlLabel sx={{
+                display: "inline-block",
+                flexShrink: 0,
+                flexGrow: 0,
+                margin: "auto 3vmin 0 0"
+              }} control={<Checkbox defaultChecked />} label="Show Intro" />
+            </Grid>
+
+            <Grid size={12}>
+              <TextField
+                autoFocus
+                margin="dense"
+                id="logo"
+                name="Logo"
+                label="Logo"
+                type="text"
+                fullWidth
+                variant="standard"
+              />
+            </Grid>
+            <Grid size={12}>
+              <TextField
+                autoFocus
+                margin="dense"
+                id="crawl"
+                name="Crawl"
+                label="Crawl"
+                type="text"
+                multiline
+                fullWidth
+                variant="standard"
+              />
+
+            </Grid>
+
+            <Grid size={12}>
+              <TextField
+                autoFocus
+                margin="dense"
+                id="music"
+                name="Music"
+                label="Music"
+                type="text"
+                fullWidth
+                variant="standard"
+              />
+            </Grid>
+          </Grid>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={() => setOpen(false)}>Cancel</Button>
+          <Button>Ok</Button>
+        </DialogActions>
+      </Dialog>
     </div>
   );
 }
